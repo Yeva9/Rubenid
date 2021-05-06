38e698f5e7b8b5db61de2d2b68fe69aed59579a0
HakobyAnna <annnahakopyan@gmail.com>
2021-05-06 09:29:59 +0400

    Fixed codding style.

--- src/SortingAlgorithms.cpp
+++ src/SortingAlgorithms.cpp
@@ -15,38 +15,37 @@ void Swap(int& a, int& b) {
 }	
 
 void BubbleSortByRubenid(int size, int* array) {
- for(int i = 0; i < size; ++i) {
+ 	for (int i = 0; i < size; ++i) {
             bool swap = false;
-            for(int j = 0; j < size - i - 1; ++j) {
-                if(array[j] > array[j + 1]) {
+            for (int j = 0; j < size - i - 1; ++j) {
+                if (array[j] > array[j + 1]) {
                     Swap(array[j + 1], array[j]);
                     swap = true;
                 }
             }
-            if(swap == false) {
+            if (swap == false) {
                 break;
             }
-        }
+	}
 }
 
 void SelectionSortByRubenid(int size, int* array) {
-	for(int i = 0; i < size; ++i) {
+	for (int i = 0; i < size; ++i) {
             int min_value_index = i;
-            for(int j = i; j < size; ++j) {
-                if(array[j] < array[min_value_index]) {
+            for (int j = i; j < size; ++j) {
+                if (array[j] < array[min_value_index]) {
                     min_value_index = j;
                 }
             }
             Swap(array[i], array[min_value_index]);
         }
-    }
-
+}
 
 void InsertionSortByRubenid(int size, int* array) {
-	for(int i = 1; i < size; ++i) {
+	for (int i = 1; i < size; ++i) {
             int key = array[i];
             int j = i - 1;
-            while(j >= 0 && array[j] > key) {
+            while (j >= 0 && array[j] > key) {
                 array[j + 1] = array[j];
                 --j;
             }
@@ -54,12 +53,12 @@ void InsertionSortByRubenid(int size, int* array) {
         }
 }
 
-
 int Partitioning(int* array, int start, int end) {
 	int  pivot = array[end];
         int i = start - 1;
-        for(int j = start; j <= end; ++j) {
-            if(array[j] < pivot) {
+        
+	for (int j = start; j <= end; ++j) {
+            if (array[j] < pivot) {
                 ++i;
                 Swap(array[i], array[j]);
             }
@@ -69,7 +68,7 @@ int Partitioning(int* array, int start, int end) {
 }
 
 void QuickSortHelper(int* array, int start, int end) {
-	if(start <= end) {
+	if (start <= end) {
             int position = Partitioning(array, start, end);
             Partitioning(array, start, position - 1);
             Partitioning(array, position + 1, end);
@@ -80,13 +79,12 @@ void QuickSortByRubenid(int size, int* array) {
     QuickSortHelper(array, 0, size - 1);
 }
 
- void MergeHelper(int* array, int* array1, int* array2, int n1, int n2, int start)
-    {
-        int i = 0;
+void MergeHelper(int* array, int* array1, int* array2, int n1, int n2, int start) {
+	int i = 0;
         int j = 0;
         int k = start;
-        while(i < n1 && j < n2) {
-            if(array1[i] <= array2[j]) {
+        while (i < n1 && j < n2) {
+            if (array1[i] <= array2[j]) {
                 array[k] = array1[i];
                 ++i;
             } else {
@@ -95,27 +93,28 @@ void QuickSortByRubenid(int size, int* array) {
             }
             ++k;
         }
-        while(i < n1) {
+        while (i < n1) {
             array[k] = array1[i];
             ++i;
             ++k;
         }
-        while(j < n2) {
+        while (j < n2) {
             array[k] = array2[j];
             ++j;
             ++k;
         }
-    }
+}
 
 void Merge(int* array, int start, int middle, int end) {
 	int n1 = middle - start + 1;
         int n2 = end - middle;
         int* array1 = new int[n1];
         int* array2 = new int[n2];
-        for(int i = 0; i < n1; ++i) {
+        
+	for (int i = 0; i < n1; ++i) {
             array1[i] = array[start + i];
         }
-        for(int j = 0; j < n2; ++j) {
+        for (int j = 0; j < n2; ++j) {
             array2[j] = array[middle + 1 + j];
         }
         MergeHelper(array, array1, array2, n1, n2, start);
@@ -124,53 +123,51 @@ void Merge(int* array, int start, int middle, int end) {
     }
     
 void MergeSortHelper(int* array, int start, int end) {
-	if(start < end) {
+	if (start < end) {
             int mid = start + (end - start) / 2;
             MergeSortHelper(array, start, mid);
             MergeSortHelper(array, mid + 1, end);
             Merge(array, start, mid, end);
         }
-    }
-
+}
 
 void MergeSortByRubenid(int size, int* array) {
     MergeSortHelper(array, 0, size - 1);
 }
 
-
 void Heapify(int* array, int size, int root) {
 	int largest = root;
         int left = 2 * root + 1;
         int right = 2 * root + 2;
-        if(left < size && array[left] > array[largest]) {
+
+	if (left < size && array[left] > array[largest]) {
             largest = left;
         }
-        if(right < size && array[right] > array[largest]) {
+        if (right < size && array[right] > array[largest]) {
             largest = right;
         }
-        if(largest != root) {
+        if (largest != root) {
             Swap(array[largest], array[root]);
             Heapify(array, size, largest);
         }
-	}
-
+}
 
 void HeapSortByRubenid(int size, int* array) {
-       for(int i = size / 2 - 1; i >= 0; --i) {
-            Heapify(array, size, i);
-        }
-        for(int i = size - 1; i >= 0; --i) {
+	for (int i = size / 2 - 1; i >= 0; --i) {
+	    Heapify(array, size, i);
+       	}
+        for (int i = size - 1; i >= 0; --i) {
             Swap(array[0], array[i]);
             Heapify(array, i, 0);
         }
 }
 
 void ShellSortByRubenid(int size, int* array) {
-for(int interval = size / 2; interval > 0; interval /= 2) {
-            for(int i = interval; i < size; ++i) {
+	for (int interval = size / 2; interval > 0; interval /= 2) {
+            for (int i = interval; i < size; ++i) {
                 int temp = array[i];
                 int j;
-                for(j = i; j >= interval && array[j - interval] > temp; j -= interval) {
+                for (j = i; j >= interval && array[j - interval] > temp; j -= interval) {
                     array[j] = array[j - interval];
                 }
                 array[j] = temp;
